310b7bcc616637139a18ead171cb0c6ddfff3f47
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2022-11-21 20:41:34 -0500

    finished crud app for the full project

--- script.ts
+++ script.ts
@@ -1,102 +1,196 @@
-const myForm = document.querySelector('#toDoForm') as HTMLFormElement; // HTMLFormElement
-const myTableContainer = document.querySelector('#toDoContainer') as HTMLElement;
-interface IToDoItem{
-    toDoName:string;
-    toDoDescription:string;
+const myForm = document.querySelector("#toDoForm") as HTMLFormElement; // HTMLFormElement
+const myTableContainer = document.querySelector(
+  "#toDoContainer"
+) as HTMLElement;
+interface IToDoItem {
+  toDoName: string;
+  toDoDescription: string;
+  id:string;
 }
 
-interface IToDoItemCls extends IToDoItem{
-    id:string;
+interface IToDoItemCls extends IToDoItem {
+  id: string;
 }
-
-
-
+class ToDoItem {
+  id:string;
+  name:string;
+  description:string;
+  parentEl:HTMLElement;
+  parentClassRef:ToDoList;
+  constructor(id:string, name:string, description:string, parentEl:HTMLElement, parentRef:ToDoList){
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.parentEl = parentEl; 
+    this.parentClassRef = parentRef;
+  }
+  render(){
+    const {newHtml, ref} = this.generateHTML();
+    if(ref){
+      this.parentEl.replaceChild(newHtml, ref);
+      return this.addEventListeners();
+    }
+    this.parentEl.appendChild(newHtml)
+    this.addEventListeners();
+  }
+  update(name:string, description:string){
+    this.name = name;
+    this.description = description;
+    this.render();
+    this.parentClassRef.updateLocalStorage();
+    
+  }
+  generateHTML(){
+    const ref = document.querySelector(`#${this.id}`); 
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('card', 'p-4', 'mt-3');
+    newDiv.id = this.id;
+    const innerHtmlString = `
+              <ul class="nav nav-pills d-flex justify-content-between">
+                <div class="d-flex">
+                  <li class="nav-item">
+                    <a
+                      class="nav-link active"
+                      data-bs-toggle="pill"
+                      href="#todo-${this.id}"
+                      >ToDo</a
+                    >
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" data-bs-toggle="pill" href="#edit-${this.id}"
+                      >Update</a
+                    >
+                  </li>
+                </div>
+                <button id="delete-${this.id}" class="btn btn-danger">Delete</button>
+              </ul>
+              <div class="tab-content">
+                <div class="tab-pane mt-4 active" id="todo-${this.id}">
+                  <div class="card">
+                    <div class="card-body">
+                      <h5 class="card-title">${this.name}</h5>
+                      <p class="card-text">${this.description}</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="tab-pane container fade" id="edit-${this.id}">
+                  <form class="card container mt-4 py-4" id="update-${this.id}">
+                    <div class="mb-3">
+                      <label for="exampleFormControlInput1" class="form-label"
+                        >To Do</label
+                      >
+                      <input
+                        type="text"
+                        class="form-control"
+                        name="name" value="${this.name}" />
+                    </div>
+                    <div class="mb-3">
+                      <label
+                        for=""
+                        class="form-label"
+                        ></label
+                      >
+                      <textarea
+                        class="form-control"
+                        name="description"
+                        rows="3">${this.description}</textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                  </form>
+                </div>
+              </div>
+            `
+    newDiv.innerHTML = innerHtmlString;
+              
+    return{
+      newHtml:newDiv,
+      ref:ref,
+    }
+  }
+  addEventListeners(){
+    const deleteButton = document.querySelector(`#delete-${this.id}`);
+    console.log(deleteButton)
+    const updateForm = document.querySelector(`#update-${this.id}`);
+    deleteButton?.addEventListener('click', ()=>{
+      myToDoList.removeToDoItem(this.id);
+    });
+    updateForm?.addEventListener('submit', (e)=>{
+      e.preventDefault();
+      const myForm = e.target as HTMLFormElement;
+      const { name, description } = Object.fromEntries(
+        new FormData(myForm) as unknown as Iterable<
+          [{ name: string; descrption: string }, FormDataEntryValue]
+        >
+      ); ;
+      this.update(name, description);
+    })
+  }
+}
 class ToDoList {
-  toDoList: IToDoItemCls[];
+  toDoList: ToDoItem[];
   container: HTMLElement;
-  callback: (data: IToDoItemCls, action: string, target: HTMLElement) => void;
-  constructor(
-    tableElement: HTMLElement,
-    callback: (data: IToDoItemCls, action: string, target: HTMLElement) => void,
-    initialToDos: IToDoItemCls[] | string = ''
-  ) {
-    this.toDoList = [];
-    if(initialToDos.length > 0 && typeof initialToDos != 'string'){
-      this.toDoList = [...initialToDos]
-    }
-    else{
-
-      this.toDoList = []
-    }
-
-    this.container = tableElement;
-    this.callback = callback;
+  constructor(container: HTMLElement, defaultValues:IToDoItem[] = []) {
+    this.container = container;
+    this.toDoList = defaultValues.map((todo) => {
+      return new ToDoItem(
+        todo.id,
+        todo.toDoName,
+        todo.toDoDescription,
+        myTableContainer,
+        this
+      );
+    });
   }
   addToDoItem(toDoItem: IToDoItemCls) {
-    this.toDoList.push(toDoItem);
-    this.callback(toDoItem, "add", this.container);
+    const newTodo = new ToDoItem(
+      toDoItem.id,
+      toDoItem.toDoName,
+      toDoItem.toDoDescription,
+      this.container,
+      this
+    );
+    this.toDoList.push(newTodo);
+    this.updateLocalStorage();
+    this.render();
   }
   removeToDoItem(id: string) {
-    const index = this.toDoList.findIndex((element) => element.id === id, 0);
-    if (index > -1) {
-      this.callback(this.toDoList[index], "remove", this.container);
-      this.toDoList.splice(index, 1);
-    }
+    const removeThis = document.querySelector(`#${id}`);
+    removeThis?.remove();
+    const newTodos = this.toDoList.filter((todo) => todo.id != id);
+    this.toDoList = newTodos;
+    this.updateLocalStorage();
+  }
+  updateLocalStorage(){
+    console.log(this.toDoList);
+    const jsonString = this.toDoList.map(({ id, name, description }) => {
+      return { id, toDoName:name, toDoDescription:description };
+    });
+        localStorage.setItem("todos", JSON.stringify(jsonString));
+
+  }
+  render() {
+    this.toDoList.forEach((todo) => {
+      todo.render();
+    });
   }
 }
-const createElement = function (item: IToDoItemCls): HTMLElement {
-  const container = document.createElement("li");
-  container.classList.add("list-group-item","d-flex", "justify-content-between", "align-items-center");
-  const { id, toDoDescription, toDoName } = item;
-  container.setAttribute("data-id", id);
-  const construtor = `<div
-             "
-              class="list-group-item w-100"
-              >
-              <div class="d-flex w-100 justify-content-between">
-                <h5 class="mb-1">${toDoName}</h5>
-                <button data-delete="${id}" class="btn btn-danger">Delete</small>
-              </div>
-              <p class="mb-1">${toDoDescription}</p>
-              
-            </div>`;
-            container.innerHTML = construtor;
 
+const currentTodos = localStorage.getItem('todos'); 
 
-  return container;
-};
-const handleUpdateToDos = (
-  data: IToDoItemCls,
-  action: string,
-  target: HTMLElement
-): void => {
-  switch (action) {
-    case "add":
-      target.appendChild(createElement(data));
-      document.querySelector(`[data-delete="${data.id}"]`)?.addEventListener('click', ()=>{
-        myToDoList.removeToDoItem(data.id);
-      })
-      break;
-    case "remove":
-        document.querySelector(`[data-id="${data.id}"]`)?.remove();
-        break;
-    default:
-      return;
-
-  }
-};
-const myToDoList = new ToDoList(myTableContainer, handleUpdateToDos);
-myForm?.addEventListener('submit',(event) => {
-    event.preventDefault();
-    const myFormData = new FormData(myForm) as unknown as Iterable<
-      [IToDoItem, FormDataEntryValue]
-    >;
-    const myFormDataObject:IToDoItem = Object.fromEntries(myFormData) ;
-    const objectToAdd:IToDoItemCls = {
-        toDoName:myFormDataObject.toDoName,
-        toDoDescription:myFormDataObject.toDoDescription,
-        id: Date.now().toString(36) + Math.random().toString(36).substring(2)
-    }
-    myToDoList.addToDoItem(objectToAdd);
-    myForm.reset();
-})
+const toDosParsed = currentTodos? JSON.parse(currentTodos) as IToDoItem[] : [];
+const myToDoList = new ToDoList(myTableContainer, toDosParsed);
+myToDoList.render();
+myForm?.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const myFormData = new FormData(myForm) as unknown as Iterable<
+    [IToDoItem, FormDataEntryValue]
+  >;
+  const myFormDataObject: IToDoItem = Object.fromEntries(myFormData);
+  const objectToAdd: IToDoItemCls = {
+    toDoName: myFormDataObject.toDoName,
+    toDoDescription: myFormDataObject.toDoDescription,
+    id: Date.now().toString(36) + Math.random().toString(36).substring(2),
+  };
+  myToDoList.addToDoItem(objectToAdd);
+  myForm.reset();
+});
--- src/script.js
+++ src/script.js
@@ -1,74 +1,156 @@
 "use strict";
-const myForm = document.querySelector('#toDoForm'); // HTMLFormElement
-const myTableContainer = document.querySelector('#toDoContainer');
-class ToDoList {
-    constructor(tableElement, callback, initialToDos = '') {
-        this.toDoList = [];
-        if (initialToDos.length > 0 && typeof initialToDos != 'string') {
-            this.toDoList = [...initialToDos];
-        }
-        else {
-            this.toDoList = [];
-        }
-        this.container = tableElement;
-        this.callback = callback;
+const myForm = document.querySelector("#toDoForm"); // HTMLFormElement
+const myTableContainer = document.querySelector("#toDoContainer");
+class ToDoItem {
+    constructor(id, name, description, parentEl, parentRef) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.parentEl = parentEl;
+        this.parentClassRef = parentRef;
     }
-    addToDoItem(toDoItem) {
-        this.toDoList.push(toDoItem);
-        this.callback(toDoItem, "add", this.container);
-    }
-    removeToDoItem(id) {
-        const index = this.toDoList.findIndex((element) => element.id === id, 0);
-        if (index > -1) {
-            this.callback(this.toDoList[index], "remove", this.container);
-            this.toDoList.splice(index, 1);
+    render() {
+        const { newHtml, ref } = this.generateHTML();
+        if (ref) {
+            this.parentEl.replaceChild(newHtml, ref);
+            return this.addEventListeners();
         }
+        this.parentEl.appendChild(newHtml);
+        this.addEventListeners();
+    }
+    update(name, description) {
+        this.name = name;
+        this.description = description;
+        this.render();
+        this.parentClassRef.updateLocalStorage();
+    }
+    generateHTML() {
+        const ref = document.querySelector(`#${this.id}`);
+        const newDiv = document.createElement('div');
+        newDiv.classList.add('card', 'p-4', 'mt-3');
+        newDiv.id = this.id;
+        const innerHtmlString = `
+              <ul class="nav nav-pills d-flex justify-content-between">
+                <div class="d-flex">
+                  <li class="nav-item">
+                    <a
+                      class="nav-link active"
+                      data-bs-toggle="pill"
+                      href="#todo-${this.id}"
+                      >ToDo</a
+                    >
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" data-bs-toggle="pill" href="#edit-${this.id}"
+                      >Update</a
+                    >
+                  </li>
+                </div>
+                <button id="delete-${this.id}" class="btn btn-danger">Delete</button>
+              </ul>
+              <div class="tab-content">
+                <div class="tab-pane mt-4 active" id="todo-${this.id}">
+                  <div class="card">
+                    <div class="card-body">
+                      <h5 class="card-title">${this.name}</h5>
+                      <p class="card-text">${this.description}</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="tab-pane container fade" id="edit-${this.id}">
+                  <form class="card container mt-4 py-4" id="update-${this.id}">
+                    <div class="mb-3">
+                      <label for="exampleFormControlInput1" class="form-label"
+                        >To Do</label
+                      >
+                      <input
+                        type="text"
+                        class="form-control"
+                        name="name" value="${this.name}" />
+                    </div>
+                    <div class="mb-3">
+                      <label
+                        for=""
+                        class="form-label"
+                        ></label
+                      >
+                      <textarea
+                        class="form-control"
+                        name="description"
+                        rows="3">${this.description}</textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                  </form>
+                </div>
+              </div>
+            `;
+        newDiv.innerHTML = innerHtmlString;
+        return {
+            newHtml: newDiv,
+            ref: ref,
+        };
+    }
+    addEventListeners() {
+        const deleteButton = document.querySelector(`#delete-${this.id}`);
+        console.log(deleteButton);
+        const updateForm = document.querySelector(`#update-${this.id}`);
+        deleteButton === null || deleteButton === void 0 ? void 0 : deleteButton.addEventListener('click', () => {
+            myToDoList.removeToDoItem(this.id);
+        });
+        updateForm === null || updateForm === void 0 ? void 0 : updateForm.addEventListener('submit', (e) => {
+            e.preventDefault();
+            const myForm = e.target;
+            const { name, description } = Object.fromEntries(new FormData(myForm));
+            ;
+            this.update(name, description);
+        });
     }
 }
-const createElement = function (item) {
-    const container = document.createElement("li");
-    container.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center");
-    const { id, toDoDescription, toDoName } = item;
-    container.setAttribute("data-id", id);
-    const construtor = `<div
-             "
-              class="list-group-item w-100"
-              >
-              <div class="d-flex w-100 justify-content-between">
-                <h5 class="mb-1">${toDoName}</h5>
-                <button data-delete="${id}" class="btn btn-danger">Delete</small>
-              </div>
-              <p class="mb-1">${toDoDescription}</p>
-              
-            </div>`;
-    container.innerHTML = construtor;
-    return container;
-};
-const handleUpdateToDos = (data, action, target) => {
-    var _a, _b;
-    switch (action) {
-        case "add":
-            target.appendChild(createElement(data));
-            (_a = document.querySelector(`[data-delete="${data.id}"]`)) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => {
-                myToDoList.removeToDoItem(data.id);
-            });
-            break;
-        case "remove":
-            (_b = document.querySelector(`[data-id="${data.id}"]`)) === null || _b === void 0 ? void 0 : _b.remove();
-            break;
-        default:
-            return;
+class ToDoList {
+    constructor(container, defaultValues = []) {
+        this.container = container;
+        this.toDoList = defaultValues.map((todo) => {
+            return new ToDoItem(todo.id, todo.toDoName, todo.toDoDescription, myTableContainer, this);
+        });
     }
-};
-const myToDoList = new ToDoList(myTableContainer, handleUpdateToDos);
-myForm === null || myForm === void 0 ? void 0 : myForm.addEventListener('submit', (event) => {
+    addToDoItem(toDoItem) {
+        const newTodo = new ToDoItem(toDoItem.id, toDoItem.toDoName, toDoItem.toDoDescription, this.container, this);
+        this.toDoList.push(newTodo);
+        this.updateLocalStorage();
+        this.render();
+    }
+    removeToDoItem(id) {
+        const removeThis = document.querySelector(`#${id}`);
+        removeThis === null || removeThis === void 0 ? void 0 : removeThis.remove();
+        const newTodos = this.toDoList.filter((todo) => todo.id != id);
+        this.toDoList = newTodos;
+        this.updateLocalStorage();
+    }
+    updateLocalStorage() {
+        console.log(this.toDoList);
+        const jsonString = this.toDoList.map(({ id, name, description }) => {
+            return { id, toDoName: name, toDoDescription: description };
+        });
+        localStorage.setItem("todos", JSON.stringify(jsonString));
+    }
+    render() {
+        this.toDoList.forEach((todo) => {
+            todo.render();
+        });
+    }
+}
+const currentTodos = localStorage.getItem('todos');
+const toDosParsed = currentTodos ? JSON.parse(currentTodos) : [];
+const myToDoList = new ToDoList(myTableContainer, toDosParsed);
+myToDoList.render();
+myForm === null || myForm === void 0 ? void 0 : myForm.addEventListener("submit", (event) => {
     event.preventDefault();
     const myFormData = new FormData(myForm);
     const myFormDataObject = Object.fromEntries(myFormData);
     const objectToAdd = {
         toDoName: myFormDataObject.toDoName,
         toDoDescription: myFormDataObject.toDoDescription,
-        id: Date.now().toString(36) + Math.random().toString(36).substring(2)
+        id: Date.now().toString(36) + Math.random().toString(36).substring(2),
     };
     myToDoList.addToDoItem(objectToAdd);
     myForm.reset();
